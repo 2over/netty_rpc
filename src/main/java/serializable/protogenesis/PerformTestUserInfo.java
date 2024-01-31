f9ade4c8ad08161a3203065fa90c6e5075df8cb9
xiehao <877664380@qq.com>
2024-01-31 14:18:10 +0800

    test commit

--- src/main/java/serializable/protogenesis/PerformTestUserInfo.java
+++ src/main/java/serializable/protogenesis/PerformTestUserInfo.java
@@ -5,5 +5,6 @@ public class PerformTestUserInfo {
     public static void main(String[] args) {
         UserInfo info = new UserInfo();
         info.setUserID(100).setUserName("Hello World");
+        // ..
     }
 }
